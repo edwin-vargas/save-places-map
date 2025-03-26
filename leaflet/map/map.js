d5fe0a4db58e363b719f1810fd3514a593220641
edwin-vargas <edwin548vargas@gmail.com>
2025-03-26 01:07:14 -0600

    suggestions for team

--- leaflet/map/map.js
+++ leaflet/map/map.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([18.008205156583344, -102.2420335255941], 15);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
